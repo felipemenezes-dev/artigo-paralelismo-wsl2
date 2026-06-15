8460993e417353d89124e248b0ad152d4c265daa
Felipe <fem@cesar.school>
2026-06-15 14:20:26 -0300

    script benchmark

---
+++ executar_sysbench.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "threads,events_per_second" > resultados.csv
+
+for t in 1 2 4 8
+do
+    for i in {1..10}
+    do
+        sysbench cpu \
+        --threads=$t \
+        --cpu-max-prime=20000 \
+        run | grep "events per second" | awk -v th=$t '{print th "," $4}' >> resultados.csv
+    done
+done
